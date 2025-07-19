2369db96850795ee4636f9588003bb1796f24997
sumonDev69 <sumonmondal.aiub.cse@gmail.com>
2025-07-19 22:55:42 +0600

    Initialize filemanager-backend project with TypeScript, Express, and Mongoose setup. Added user model and basic configuration files.

---
+++ src/models/user.model.ts
@@ -0,0 +1,31 @@
+import {Schema , model, Document} from 'mongoose';
+
+export interface IUser extends Document {
+  googleId: string;
+  displayName: string;
+  email: string;
+  avatar: string;
+}
+
+const UserSchema = new Schema<IUser>({
+    googleId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    displayName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    avatar: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });  
+
+export default model<IUser>('User', UserSchema);
